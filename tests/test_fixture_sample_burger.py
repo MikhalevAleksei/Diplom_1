20aa7ab7e4fd996239ba1e42f12f40bad8566f03
Aleksei Mikhalev <118346927+MikhalevAleksei@users.noreply.github.com>
2024-01-10 11:17:34 +0300

    Diplom 1 try 2

--- tests/test_fixture_sample_burger.py
+++ tests/test_fixture_sample_burger.py
@@ -1,8 +1,28 @@
-def test_sample_burger(sample_burger):
-    assert sample_burger.bun.get_name() == "Sample Bun"
-    assert sample_burger.bun.get_price() == 200.00
-    assert len(sample_burger.ingredients) == 2
-    assert sample_burger.ingredients[0].get_name() == "Ingredient1"
-    assert sample_burger.ingredients[0].get_price() == 100.00
-    assert sample_burger.ingredients[1].get_name() == "Ingredient2"
-    assert sample_burger.ingredients[1].get_price() == 150.00
+class TestSampleBurger:
+    def test_sample_burger_name(self, sample_burger):
+        name = sample_burger.bun.get_name()
+        assert name == "Sample Bun"
+
+    def test_sample_burger_price(self, sample_burger):
+        price = sample_burger.bun.get_price()
+        assert price == 200.00
+
+    def test_sample_burger_quantity(self, sample_burger):
+        quantity = len(sample_burger.ingredients)
+        assert quantity == 2
+
+    def test_sample_burger_name_of_first_ingredient(self, sample_burger):
+        name_of_first_ingredient = sample_burger.ingredients[0].get_name()
+        assert name_of_first_ingredient == "Ingredient1"
+
+    def test_sample_burger_price_of_first_ingredient(self, sample_burger):
+        price_of_first_ingredient = sample_burger.ingredients[0].get_price()
+        assert price_of_first_ingredient == 100.00
+
+    def test_sample_burger_name_of_second_ingredient(self, sample_burger):
+        second_ingredient = sample_burger.ingredients[1].get_name()
+        assert second_ingredient == "Ingredient2"
+
+    def test_sample_burger_price_of_second_ingredient(self, sample_burger):
+        price_of_second_ingredient = sample_burger.ingredients[1].get_price()
+        assert price_of_second_ingredient == 150.00
