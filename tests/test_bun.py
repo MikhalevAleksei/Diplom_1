c1dfdb0904ca8d681f0dbbef235b4710b60637ea
Aleksei Mikhalev <118346927+MikhalevAleksei@users.noreply.github.com>
2024-02-10 19:34:18 +0300

    Update test_bun.py

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -5,9 +5,9 @@ class TestBun:
     bun = Bun('Big_bun', 50)
 
     def test_get_name(self):
-        bun_name = TestBun.bun.get_name()
+        bun_name = self.bun.get_name()
         assert bun_name == 'Big_bun'
 
     def test_get_price(self):
-        bun_price = TestBun.bun.get_price()
+        bun_price = self.bun.get_price()
         assert bun_price == 50
